a9927ccb4e425800e9c858e4f53bfc17f92d9a19
User <teshbud@yandex.ru>
2022-10-12 17:39:05 +0300

    Version 1.1. add bot commands

--- src/main/java/com/vvpanf/todolistbot/config/AppConfig.java
+++ src/main/java/com/vvpanf/todolistbot/config/AppConfig.java
@@ -5,7 +5,13 @@ import com.vvpanf.todolistbot.model.TelegramFacade;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 @Configuration
 @AllArgsConstructor
@@ -18,11 +24,19 @@ public class AppConfig {
     }
 
     @Bean
-    public TelegramBot springWebhookBot(SetWebhook setWebhook, TelegramFacade telegramFacade) {
+    public TelegramBot springWebhookBot(SetWebhook setWebhook, TelegramFacade telegramFacade) throws TelegramApiException {
         TelegramBot bot = new TelegramBot(telegramFacade, setWebhook);
         bot.setBotToken(botConfig.getBotToken());
         bot.setBotUsername(botConfig.getUserName());
         bot.setBotPath(botConfig.getWebHookPath());
+        List<BotCommand> commands = List.of(
+            new BotCommand("show", "посмотреть свои списки"),
+            new BotCommand("create", "создать новый список"),
+            new BotCommand("delete", "удалить список"),
+            new BotCommand("help", "справка")
+        );
+        SetMyCommands cmds = new SetMyCommands(commands, new BotCommandScopeDefault(),  "ru");
+        bot.execute(cmds);
         return bot;
     }
 }
